dbb3ab59f10455aea8d9de37127ba04f2c0c7688
rushin-gh <rushin.mailme@gmail.com>
2025-06-25 21:35:40 +0530

    Basic operators

---
+++ BitManipulation.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    AND
+    0 & 0 = 0
+    0 & 1 = 0
+    1 & 0 = 0
+    1 & 1 = 1
+
+    OR
+    0 | 0 = 0
+    0 | 1 = 1
+    1 | 0 = 1
+    1 | 1 = 1
+
+    XOR
+    0 | 0 = 0
+    0 | 1 = 1
+    1 | 0 = 1
+    1 | 1 = 0;
+
+    NOT
+    ~0 = 1
+    ~1 = 0
+
+    LEFT SHIFT
+    (y << x) -> (y * 2^x) -> x number of 0s added in bit representation of y
+
+    RIGHT SHIFT
+    (y >> x) -> (y / 2^x) -> x number of last bits removed from bit representation of y
+*/
+
+int main() {
+
+}
